6bf55c0cd82b0dff8f1c28c1c09befe52816edfa
Leslie_Yan <leslieyan0621@gmail.com>
2016-12-05 23:31:30 +0800

    Generate score table.

--- rating/InvestorRating.py
+++ rating/InvestorRating.py
@@ -13,13 +13,26 @@ class InvestorScore(object):
 
     def generate_table(self):
         self.table = dict(
-            Seed = dict(Angel=0, preA=.1, A=.2, Aplus=.25, preB=.3, B=.4, Bplus=.45,
-                        C=.6, D=.75, E=.85, FbeforeIPO=.95, IPO=1, afterIPO=1),
+            Seed  = dict(Angel=0, preA=.1, A=.2, Aplus=.25, preB=.3, B=.4, Bplus=.45,
+                         C=.6, D=.75, E=.85, FbeforeIPO=.95, IPO=1, afterIPO=1),
             Angel = dict(Seed=0, preA=.1, A=.2, Aplus=.25, preB=.3, B=.4, Bplus=.45,
-                        C=.6, D=.75, E=.85, FbeforeIPO=.95, IPO=1, afterIPO=1),
-            preA = dict(A=.1, Aplus=.2, preB=.25, B=.3, Bplus=.4, C=.45, D=.6,
-                        E=.75, FbeforeIPO=.85, IPO=.95, afterIPO=.95)
-
+                         C=.6, D=.75, E=.85, FbeforeIPO=.95, IPO=1, afterIPO=1),
+            preA  = dict(A=.1, Aplus=.2, preB=.25, B=.3, Bplus=.4, C=.45, D=.6,
+                         E=.75, FbeforeIPO=.85, IPO=.95, afterIPO=.95),
+            A     = dict(Aplus=.1, preB=.2, B=.25, Bplus=.3, C=.4, D=.45, E=.6,
+                         FbeforeIPO=.75, IPO=.85, afterIPO=.85),
+            Aplus = dict(preB=.1, B=.2, Bplus=.25, C=.3, D=.4, E=.45, FbeforeIPO=.6,
+                         IPO=.75, afterIPO=.75),
+            preB  = dict(B=.1, Bplus=.2, C=.25, D=.3, E=.4, FbeforeIPO=.45,
+                         IPO=.6, afterIPO=.6),
+            B     = dict(Bplus=.1, C=.2, D=.25, E=.3, FbeforeIPO=.4, IPO=.45,
+                         afterIPO=.45),
+            Bplus = dict(C=.1, D=.2, E=.25, FbeforeIPO=3, IPO=.4, afterIPO=.4),
+            C     = dict(D=.1, E=.2, FbeforeIPO=.25, IPO=.3, afterIPO=.3),
+            D     = dict(E=.1, FbeforeIPO=.2, IPO=.25, afterIPO=.25),
+            E     = dict(FbeforeIPO=.1, IPO=.2, afterIPO=.2),
+            FbeforeIPO = dict(IPO=.1, afterIPO=.1),
+            IPO   = dict(afterIPO=.05)
         )
 
     def calculator(self, round_in, round_now):

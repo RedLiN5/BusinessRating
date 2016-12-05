3a768a99b3f2a431b1dc3d92ef2e555ef27f9d79
Leslie_Yan <leslieyan0621@gmail.com>
2016-12-05 23:18:18 +0800

    Generate score table

--- rating/InvestorRating.py
+++ rating/InvestorRating.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import pandas as pd
 
@@ -8,6 +10,17 @@ class InvestorScore(object):
         self.investor = investor
         self.rounds_sort = ['IPO上市后','IPO上市','F轮-上市前','E轮','D轮','C轮','B+轮',
                             'B轮','Pre-B轮','A+轮','A轮','Pre-A轮','天使轮','种子轮']
+
+    def generate_table(self):
+        self.table = dict(
+            Seed = dict(Angel=0, preA=.1, A=.2, Aplus=.25, preB=.3, B=.4, Bplus=.45,
+                        C=.6, D=.75, E=.85, FbeforeIPO=.95, IPO=1, afterIPO=1),
+            Angel = dict(Seed=0, preA=.1, A=.2, Aplus=.25, preB=.3, B=.4, Bplus=.45,
+                        C=.6, D=.75, E=.85, FbeforeIPO=.95, IPO=1, afterIPO=1),
+            preA = dict(A=.1, Aplus=.2, preB=.25, B=.3, Bplus=.4, C=.45, D=.6,
+                        E=.75, FbeforeIPO=.85, IPO=.95, afterIPO=.95)
+
+        )
 
     def calculator(self, round_in, round_now):
         pass

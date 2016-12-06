4e6b9036e2d6ce1dfc266a410fda825f54742baa
Leslie_Yan <leslieyan0621@gmail.com>
2016-12-07 00:28:57 +0800

    Need to dubug.

--- rating/rating.py
+++ rating/rating.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from etl import Preprocessing
+from InvestorRating import InvestorScore
 import numpy as np
 import pandas as pd
 
@@ -16,19 +17,25 @@ class GetScore(Preprocessing):
         pre = Preprocessing('../data/mydata.csv')
         pre.fit()
 
-    def search(self):
-        df = self.df
-        target_index = df['Investee'] == self.name
-        if sum(target_index) > 1:
-            investors = df[target_index]
-        elif sum(target_index) == 1:
-            round, amount = df.ix[target_index, ['FinancingRound', 'Amount']]
+    def return_score(self):
+        test = InvestorScore(investor=self.name, df=self.df)
+        score = test.start()
+        return score
+    # def search(self):
+    #     df = self.df
+    #     target_index = df['Investee'] == self.name
+    #     if sum(target_index) > 1:
+    #         investors = df[target_index]
+    #     elif sum(target_index) == 1:
+    #         round, amount = df.ix[target_index, ['FinancingRound', 'Amount']]
 
 
 ###############
+#
+# pre = Preprocessing('../data/mydata.csv')
+# df = pre.fit()
+#
+# print len(np.unique(df['Investee'])), '\n'
+# for round in np.unique(df['FinancingRound']): print round
 
-pre = Preprocessing('../data/mydata.csv')
-df = pre.fit()
-
-print len(np.unique(df['Investee'])), '\n'
-for round in np.unique(df['FinancingRound']): print round
+print GetScore('猎聘网').return_score()

56318d6d82a83738393354939b8cfd84077b5fe4
Leslie_Yan <leslieyan0621@gmail.com>
2016-12-07 14:39:05 +0800

    Rating score object finished.

--- rating/rating.py
+++ rating/rating.py
@@ -1,41 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from etl import Preprocessing
 from InvestorRating import InvestorScore
+from etl import Preprocessing
 import numpy as np
 import pandas as pd
 
 
-
-class GetScore(Preprocessing):
+class GetScore(InvestorScore):
 
     def __init__(self, name):
-        self.name = name
-
-    def preprocess(self):
-        pre = Preprocessing('../data/mydata.csv')
-        pre.fit()
+        super(GetScore,self).__init__(investor=name)
 
     def return_score(self):
-        test = InvestorScore(investor=self.name, df=self.df)
-        score = test.start()
-        return score
-    # def search(self):
-    #     df = self.df
-    #     target_index = df['Investee'] == self.name
-    #     if sum(target_index) > 1:
-    #         investors = df[target_index]
-    #     elif sum(target_index) == 1:
-    #         round, amount = df.ix[target_index, ['FinancingRound', 'Amount']]
+        return self.start()
 
 
-###############
-#
-# pre = Preprocessing('../data/mydata.csv')
-# df = pre.fit()
-#
-# print len(np.unique(df['Investee'])), '\n'
-# for round in np.unique(df['FinancingRound']): print round
-
-print GetScore('猎聘网').return_score()
+test = GetScore(name = r'猎聘网')

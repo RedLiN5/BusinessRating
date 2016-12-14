9ce6d036223a6ac4ceed32897a87feef8df4df32
Leslie_Yan <leslieyan0621@gmail.com>
2016-12-14 16:09:33 +0800

    Investors' scores normalization.

--- data/Normalization.py
+++ data/Normalization.py
@@ -1,10 +1,22 @@
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 import numpy as np
-from ..rating.etl import Preprocessing
+import sys
 
-class DataNormalization(Preprocessing):
+sys.path.append('..')
+from rating.InvestorRating import InvestorScore
+
+class DataNormalization(InvestorScore):
 
     def __init__(self):
-        super(DataNormalization, self).__init__(file='mydata.csv')
+        super(DataNormalization, self).__init__()
         self.fit()
 
+    def normal(self):
+        df = self.df.copy()
+        investors_unique = np.unique(df['Investor'].values)
+        scores = list(map(self.investor_score, investors_unique))
+        scores_normal = (scores - np.mean(scores))/(max(scores) - min(scores))
+        investor_score = pd.DataFrame({'Investor': investors_unique, 'Score': scores_normal})
+        return investor_score

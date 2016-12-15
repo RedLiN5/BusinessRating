708fcc5b281e87bcae6007b6bb17de9fd510c66b
Leslie_Yan <leslieyan0621@gmail.com>
2016-12-15 16:14:45 +0800

    Improvement: associated with Normalization

--- rating/InvesteeRating.py
+++ rating/InvesteeRating.py
@@ -2,30 +2,39 @@
 
 import numpy as np
 import pandas as pd
-from InvestorRating import InvestorScore
-from etl import Preprocessing
+import sys
+sys.path.append('..')
 
-class InvesteeScore(InvestorScore, Preprocessing):
+from rating.InvestorRating import InvestorScore
+from rating.etl import Preprocessing
+from data.Normalization import DataNormalization
+
+class InvesteeScore(DataNormalization):
 
     def __init__(self, investee):
         self.investee = investee
-        InvestorScore.__init__(self)
-        Preprocessing.__init__(self, file='../data/mydata.csv', sep = ',', header = 0, index_col = 0)
+        Preprocessing.__init__(self, file='data/mydata.csv', sep = ',', header = 0, index_col = 0)
         self.fit()
+        DataNormalization.__init__(self)
+        self.normalization()
 
     def investee_final_score(self):
         df = self.df
         investee_ind = df.Investee == self.investee
         investee_investors = df.ix[investee_ind, 'Investor'].values
+        normal_score = self.normal_score
 
         scores = []
         n = 0
         for investor in investee_investors:
-            score = self.investor_score(investor=investor)
-            if isinstance(score, str):
-                continue
-            else:
-                n += 1
+            try:
+                investor in normal_score.Investor
+                investor_ind = normal_score.Investor == investor
+                score = normal_score.ix[investor_ind, 'Score']
                 scores += [score]
+                n += 1
+            except:
+                print(investor, 'info cannot currently be obtained.')
+
         score_mean = sum(scores)/float(n)
         return score_mean

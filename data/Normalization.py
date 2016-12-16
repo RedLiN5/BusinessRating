Fix: improve normalization.

data/Normalization.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 import sys
+import math
 
 sys.path.append('..')
 from rating.InvestorRating import InvestorScore
@@ -20,7 +21,10 @@ class DataNormalization(InvestorScore):
         df_unique = pd.DataFrame({'Investor': investors_unique, 'Score': scores})
         df_new = df_unique.loc[df_unique.Score != -1]
         scores_new = df_new.Score
-        scores_normal = (scores_new - np.mean(scores_new))/(max(scores_new) - min(scores_new))
-        scores_normal = scores_normal + abs(min(scores_normal))
+        scores_mean = np.mean(scores_new)
+        scores_std = np.std(scores_new)
+        n = len(scores_new)
+        scores_normal = (scores_new - scores_mean)/(scores_std/math.sqrt(n))
+        scores_normal = scores_normal + 0.5
         df_new.Score = scores_normal
         self.normal_score = df_new
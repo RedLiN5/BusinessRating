09378935f0e50718f25d37d07690db6467f0e66a
Leslie_Yan <leslieyan0621@gmail.com>
2016-12-16 23:03:31 +0800

    Improvement: Code tuned according to Pylint score.

--- data/Normalization.py
+++ data/Normalization.py
@@ -1,30 +1,52 @@
 # -*- coding: utf-8 -*-
+"""
+NAME
+    normalization
 
+DESCRIPTION
+    =====
+
+    Provides
+      Normalization for investor scores.
+
+"""
+
+import math
 import pandas as pd
 import numpy as np
-import sys
-import math
-
-sys.path.append('..')
 from rating.InvestorRating import InvestorScore
 
+
 class DataNormalization(InvestorScore):
+    """
+    Methods defined here:
+
+    normalization(self)
+        Process if data frame from InvestorScore exists.
+    """
 
     def __init__(self):
         super(DataNormalization, self).__init__()
         self.fit()
+        self.normal_score = None
 
     def normalization(self):
-        df = self.df.copy()
-        investors_unique = np.unique(df['Investor'].values)
+        """
+        normalization(...)
+
+        Run normalization process.
+        :return:
+        """
+        dataframe = self.df.copy()
+        investors_unique = np.unique(dataframe['Investor'].values)
         scores = list(map(self.investor_score, investors_unique))
         df_unique = pd.DataFrame({'Investor': investors_unique, 'Score': scores})
         df_new = df_unique.loc[df_unique.Score != -1]
         scores_new = df_new.Score
         scores_mean = np.mean(scores_new)
         scores_std = np.std(scores_new)
-        n = len(scores_new)
-        scores_normal = (scores_new - scores_mean)/(scores_std/math.sqrt(n))
+        scores_length = len(scores_new)
+        scores_normal = (scores_new - scores_mean)/(scores_std/math.sqrt(scores_length))
         scores_normal = scores_normal + 0.5
         df_new.Score = scores_normal
         self.normal_score = df_new

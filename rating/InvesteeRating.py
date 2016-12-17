f84c05424c1cb449584a0f237cdf15d3dc6c7de9
Leslie_Yan <leslieyan0621@gmail.com>
2016-12-17 14:50:44 +0800

    Improvement: Codes tuned according to Pylint score.

--- rating/InvesteeRating.py
+++ rating/InvesteeRating.py
@@ -1,40 +1,62 @@
 # -*- coding: utf-8 -*-
+"""
+NAME
+    InvesteeRating
 
-import numpy as np
-import pandas as pd
-import sys
-sys.path.append('..')
+DESCRIPTION
+    This module only includes the method to calculate an investee's score
 
-from rating.InvestorRating import InvestorScore
+CLASS
+    InvesteeRating.InvesteeScore
+
+"""
+
 from rating.etl import Preprocessing
 from data.Normalization import DataNormalization
 
 class InvesteeScore(DataNormalization):
+    """
+    Calculate an investee's score based on one's investors' ratings.
 
+    Parameters
+    ----------
+    investee: str
+    """
     def __init__(self, investee):
         self.investee = investee
-        Preprocessing.__init__(self, file='data/mydata.csv', sep = ',', header = 0, index_col = 0)
+        Preprocessing.__init__(self,
+                               file='data/mydata.csv',
+                               sep=',',
+                               header=0,
+                               index_col=0)
         self.fit()
         DataNormalization.__init__(self)
         self.normalization()
 
     def investee_final_score(self):
-        df = self.df
-        investee_ind = df.Investee == self.investee
-        investee_investors = df.ix[investee_ind, 'Investor'].values
+        """
+        Calculate an investee score.
+
+        Returns
+        -------
+        score_mean: int
+        """
+        data_df = self.df
+        investee_ind = data_df.Investee == self.investee
+        investee_investors = data_df.ix[investee_ind, 'Investor'].values
         normal_score = self.normal_score
 
         scores = []
-        n = 0
+        n_scores = 0
         for investor in investee_investors:
             try:
-                investor in normal_score.Investor
-                investor_ind = normal_score.Investor == investor
-                score = normal_score.ix[investor_ind, 'Score'].values[0]
-                scores.append(score)
-                n = n + 1
+                while investor in normal_score.Investor:
+                    investor_ind = normal_score.Investor == investor
+                    score = normal_score.ix[investor_ind, 'Score'].values[0]
+                    scores.append(score)
+                    n_scores = n_scores + 1
             except:
                 print('Investor %s info cannot currently be obtained.'% investor)
 
-        score_mean = sum(scores)/float(n)
+        score_mean = sum(scores)/float(n_scores)
         return score_mean
